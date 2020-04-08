35fa4c37354f6a55a256ae5c8ef2877767eb893a
Andrey Ivanov <aivanov@vp.ru>
2020-04-08 18:38:36 +0600

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+const cardRoutes = require('./routes/card');
+
+const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+
+async function start() {
+    try {
+        const user = 'admin';
+        const password = 'BiYD7SnZUCpfuaKU';
+        const url = `mongodb+srv://admin:${password}@cluster0-vmmod.mongodb.net/test?retryWrites=true&w=majority`
+
+        await mongoose.connect(url, { useNewUrlParser: true });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`)
+        });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start();
