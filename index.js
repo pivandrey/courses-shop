4390d26efaa78c0b240e728ce4426103a5971362
Andrey Ivanov <aivanov@vp.ru>
2020-04-09 20:26:39 +0600

    Add orders

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
+const ordersRoutes = require('./routes/orders');
+
 const User = require('./models/user');
 
 const app = express();
@@ -36,6 +39,7 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/orders.js
@@ -0,0 +1,55 @@
+const { Router } = require('express')
+const Order = require('../models/order');
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const orders = await Order.find({ 'user.userId': req.user._id })
+            .populate('user.userId');
+
+        res.render('orders', {
+            isOrder: true,
+            title: 'Заказы',
+            orders: orders.map(o => ({
+                ...o._doc,
+                price: o.courses.reduce((acc, c) => {
+                    return acc + c.course.price * c.count;
+                }, 0)
+            }))
+        })
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+
+        const user = await req.user
+            .populate('cart.items.courseId')
+            .execPopulate();
+
+        const courses = user.cart.items.map(c => ({
+            course: { ...c.courseId._doc },
+            count: c.count
+        }))
+
+        const order = new Order({
+            user: {
+                name: req.user.name,
+                userId: req.user
+            },
+            courses
+        })
+
+        await order.save();
+        await req.user.clearCart();
+
+        res.redirect('/orders');
+    } catch (e) {
+        console.log(e);
+    }
+
+})
+
+module.exports = router;
