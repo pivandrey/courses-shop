e514e888eb6e1543e584ef0fbde82cb3815769fd
Andrey Ivanov <aivanov@vp.ru>
2020-04-12 23:08:45 +0600

    Add confim email and reset password

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,8 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://admin:BiYD7SnZUCpfuaKU@cluster0-vmmod.mongodb.net/shop',
     PORT: process.env.PORT || 3000,
-    SESSION_SECRET: 'some secret value'
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'pivandrey777@gmail.com',
+    BASE_URL: 'http://localhost:3000'
 }
